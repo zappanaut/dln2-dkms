9e68da29b17f71ddeaee9e3a3fe9fe1ffb0f5b60
zappanaut <zappanaut@github>
2023-01-28 15:47:17 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
-cp -a src /usr/src/dln2-5.15.0
+VERSION=5.15.0
+MODULE_NAME=dln2
+
+
+#
+#  udev
+#
 cp -a udev/*.rules /etc/udev/rules.d/
 mkdir /dev/gpio-by-serial
+
+
+#
+#  dkms
+#
+DKMS_ROOT_PATH=/usr/src/${MODULE_NAME}-${VERSION}
+
+mkdir -p ${DKMS_ROOT_PATH}
+cp src/* ${DKMS_ROOT_PATH}
+
+sed -e "s/%VERSION%/${VERSION}/" -i ${DKMS_ROOT_PATH}/dkms.conf
+
+dkms add ${MODULE_NAME}/${VERSION}
+dkms build ${MODULE_NAME}/${VERSION}
+dkms install ${MODULE_NAME}/${VERSION}
+
+# uninstall
+# dkms remove ${MODULE_NAME}/${VERSION} --all
+# rm -rf ${DKMS_ROOT_PATH}
